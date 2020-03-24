973db7c4c58d1215146d44b18760db457d69212a
Alex Raubach <alex@alexraubach.com>
2020-03-23 23:26:29 -0400

    Depreciate and clean up Image Controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,17 +1,20 @@
 class ImagesController < ApplicationController
-
+  # This whole controller is depreciated and shouldn't be used.
+  # Just render your image directly.
   def show
     if params[:pilot_id].present?
-      deliver_image(Pilot.find(params[:pilot_id])) and return
+      deliver_image(Pilot.find(params[:pilot_id]))
+      return
     end
     if params[:upgrade_id].present?
       upgrade = Upgrade.find(params[:upgrade_id])
-      upgrade_side = UpgradeSide.find_by(upgrade_id:upgrade.id)
-      p upgrade_side
-      deliver_image(upgrade_side) and return
+      upgrade_side = UpgradeSide.find_by(upgrade_id: upgrade.id)
+      deliver_image(upgrade_side)
+      return
     end
     if params[:condition_id].present?
-      deliver_image(Condition.find(params[:condition_id])) and return
+      deliver_image(Condition.find(params[:condition_id]))
+      return
     end
     render nothing: true
   end
@@ -21,7 +24,6 @@ class ImagesController < ApplicationController
   def deliver_image(model)
     #root = Rails.root + 'vendor' + 'xwing-data' + 'images'
     #target = root + model.image.to_s # if nil, target will be root and thus not a file
-    p model.image.to_s
     target = model.image.to_s
     #if .cletargetanpath.to_s.include?(root.cleanpath.to_s) && target.file? # check if child of root, to avoid escaping the sandbox
     if target.present?
@@ -30,5 +32,4 @@ class ImagesController < ApplicationController
       render file: 'public/404.html', content_type: 'text/html', status: :not_found, layout: false
     end
   end
-
 end
