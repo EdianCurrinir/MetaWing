10804e261617c3f7598b129511618fe7abc1ee9d
Alex Raubach <alex@alexraubach.com>
2020-08-30 16:49:35 -0400

    Replace URI.escape

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def link_with_filter_to(path, *args, &block)
     if path.start_with?('/')
       if path.include?('?')
@@ -9,9 +8,8 @@ module ApplicationHelper
       end
     end
     raw_ranking_configuration.each do |key, value|
-      path += "#{key}=#{URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}&"
+      path += "#{key}=#{URI.encode_www_form_component(value.to_s)}&"
     end
     link_to(path, *args, &block)
   end
-
 end
