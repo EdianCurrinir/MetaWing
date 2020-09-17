43f61478dd29d09cc977c309f863911b4aed16a1
Alex Raubach <alex@alexraubach.com>
2020-09-17 17:56:52 -0400

    Fix for empty string dates causing bad sql queries
    You can select a blank option in the date filter
    and that causes an sql error.
    Switching from .nil? to .blank? fixes that problem.
    The top two options can stay as .nil? because of the
    == 'true' comparision.
    
    The format and t_type nil check wasn't being used so I removed it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
 
   def ranking_configuration
     {
-        large_tournament_multiplier: params['large_tournament_multiplier'].nil? ? true                 : (params['large_tournament_multiplier'] == 'true'),
-        widespread_use_multiplier:   params['widespread_use_multiplier'].nil?   ? true                 : (params['widespread_use_multiplier'] == 'true'),
-        use_ranking_data:            params['use_ranking_data'].nil?            ? 'all'                : params['use_ranking_data'],
-        ranking_start:               params['ranking_start'].nil?               ? 1.month.ago.to_date  : params['ranking_start'],
-        ranking_end:                 params['ranking_end'].nil?                 ? Time.current.to_date : params['ranking_end'],
-        tournament_type:             params['tournament_type'].nil?             ? nil                  : params['tournament_type'],
-        format_id:                   params['format_id'].nil?                   ? nil                  : params['format_id'],
+      large_tournament_multiplier: params['large_tournament_multiplier'].nil?   ? true                 : (params['large_tournament_multiplier'] == 'true'),
+      widespread_use_multiplier:   params['widespread_use_multiplier'].nil?     ? true                 : (params['widespread_use_multiplier'] == 'true'),
+      use_ranking_data:            params['use_ranking_data'].blank?            ? 'all'                : params['use_ranking_data'],
+      ranking_start:               params['ranking_start'].blank?               ? 1.month.ago.to_date  : params['ranking_start'],
+      ranking_end:                 params['ranking_end'].blank?                 ? Time.current.to_date : params['ranking_end'],
+      tournament_type:             params['tournament_type'],
+      format_id:                   params['format_id'],
     }
   end
   helper_method :ranking_configuration
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
         params['tournament_type'].blank?                                                                     ? nil : params['tournament_type'],
       format_id:
         params['format_id'].blank?                                                                     ? nil : params['format_id'],
-    }.reject{|_k, v| v.nil?}]
+    }.reject { |_k, v| v.nil? }]
   end
   helper_method :raw_ranking_configuration
 
