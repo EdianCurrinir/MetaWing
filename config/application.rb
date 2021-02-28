79a0da7f80c435ba5be432481ce1e8a61d5f92ea
Alex Raubach <alex@alexraubach.com>
2021-02-27 23:29:29 -0500

    Upgrade to Rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,12 +8,16 @@ Bundler.require(*Rails.groups)
 
 module ListJuggleBrowser
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
-    config.time_zone = 'Bern'
-    config.i18n.default_locale = :en
-    config.i18n.available_locales = [:en]
-    config.i18n.enforce_available_locales = true
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = "Eastern Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
   end
 end
