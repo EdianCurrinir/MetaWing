d9ded36d33a4408154f6fd2472b55e970c37e546
Alex Raubach <alex@alexraubach.com>
2021-10-21 20:38:17 -0400

    Explicitly delegate keyword argument

--- app/controllers/squadrons_controller.rb
+++ app/controllers/squadrons_controller.rb
@@ -35,11 +35,10 @@ class SquadronsController < ApplicationController
       parameter_name = "#{klass.name.underscore}_id".to_sym
       if params[parameter_name].present?
         ranker = "Rankers::#{klass.name.pluralize}Ranker".constantize
-        [klass, ranker.new(ranking_configuration, { parameter_name => params[parameter_name] })]
+        [klass, ranker.new(ranking_configuration, **{ parameter_name => params[parameter_name] })]
       else
         nil
       end
     end.compact
   end
-
 end
